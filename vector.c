045c68805c1ab867b2c92a48fa2bcd4146b92ed0
godthechaoticevil <godthechaoticevil@gmail.com>
2020-12-14 05:33:48 +0100

    fixed vector.c and added comments

--- vector.c
+++ vector.c
@@ -6,45 +6,60 @@ unsigned short capacity;
 
 void vector(unsigned short size)
 {
-	storage = calloc(size, 1);
-	if (storage == NULL) exit(1);
-	capacity = size;
+	storage = (char**)calloc(size, sizeof(char*)); //initialize storage
+	if (storage == NULL) exit(1); //check if storage was initialised correctly
+	capacity = size;  //initialized capacity
 }
 
 void push(char *string)
 {
-	for (int i = 0; i < capacity; i++){
-		if (storage[i] == NULL){
+  int c = capacity;
+
+  //loop over storage
+	for (int i = 0; i < c; i++)
+  {
+    //insert string if storage is empty at i
+		if (storage[i] == NULL)
+    {
 			storage[i] = string;
 			return;
 		}
 	}
-	int i = capacity;
-	capacity *= 2;
-	storage = realloc(storage, capacity);
-	if (storage == NULL) exit(1);
-	storage[i] = string;
+  
+  char **old = storage; //make copy of storage pointer
+	vector(2*c);  //create larger storage
+  
+  //copy elements from old to storage
+  for (int i = 0; i < c; i++) storage[i] = old[i]; 
+  storage[c] = string;  //insert string at first new index
+  free(old);  //free up old memory
 }
 
 char* at(unsigned short index)
 {
+  //check if index is legal and return corresponding element (or NULL)
 	if (index < capacity) return storage[index];
 	return NULL;
 }
 
 void set(unsigned short index, char *string)
 {
+  //check if index is legal and set string at coresponding position
 	if (index < capacity) storage[index] = string;
 	return;
 }
 
 unsigned short size(void)
 {
-	unsigned short size = 0;
-	for (int i = 0; i > capacity; i++){
-		if (storage[i] != NULL) size++;
-	}
-	return size;
+	unsigned short s = 0; //initialize temp. variable for size
+
+  //loop over entire storage array
+	for (int i = 0; i < capacity; i++)
+  {
+    //increment size if the current element exists
+    if (storage[i] != NULL) s ++;
+  }
+	return s;
 }
 
 int main(void)
